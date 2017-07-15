6457bf916ec2b64d794ab223e4b26d28a8fb6471
Takanori Suzuki <takanori@takanory.net>
2017-07-15 15:14:17 +0900

    ISSHA-618 store channels list to google spreadsheet

--- slack_channels_list.py
+++ slack_channels_list.py
@@ -9,6 +9,10 @@ from google_api import get_sheets_service
 import settings
 
 
+# チャンネル一覧を書き込むスプレッドシートのID
+SHEET_ID = '1Z93vxxC6zdSunO52-9hQpeGpBqQeFPIPLn2ayPRmtJY'
+
+
 def get_channels_list():
     '''
     Slackのチャンネル一覧から必要な情報を返す
@@ -21,7 +25,7 @@ def get_channels_list():
     for channel in r.body['channels']:
         channel_info = {
             'id': channel['id'],
-            'name': channel['name'],
+            'name': '#' + channel['name'],
             'topic': channel['topic']['value'],
             'purpose': channel['purpose']['value'],
             'members': channel['num_members'],
@@ -30,14 +34,43 @@ def get_channels_list():
     return channels
 
 
+def store(channels):
+    '''
+    Slackのチャンネル情報をGoogleスプレッドシートに書き込む
+
+    https://docs.google.com/spreadsheets/d/1Z93vxxC6zdSunO52-9hQpeGpBqQeFPIPLn2ayPRmtJY/edit#gid=0
+    '''
+
+    values = []
+    # チャンネル情報を配列にする
+    KEYS = ('name', 'id', 'members', 'purpose', 'topic')
+    for channel in channels:
+        # 辞書からKEYSに従った順番に値を取り出す
+        values.append([channel[k] for k in KEYS])
+    data = [
+        {
+            'range': 'A2:E100',
+            'values': values,
+        }
+    ]
+    body = {
+        'valueInputOption': 'RAW',
+        'data': data,
+    }
+    service = get_sheets_service()
+    result = service.spreadsheets().values().batchUpdate(
+        spreadsheetId=SHEET_ID, body=body).execute()
+    print(result)
+
+
 def main():
     '''
     Slackのチャンネル一覧情報を取得する
     '''
+    # チャンネル一覧を取得
     channels = get_channels_list()
-    for channel in channels:
-        print(channel['name'])
-    #print(channels)
+    # チャンネルの情報をスプレッドシートに書き出す
+    store(channels)
 
 
 if __name__ == '__main__':
